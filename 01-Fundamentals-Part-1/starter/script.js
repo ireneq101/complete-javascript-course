10d6a0f3280ca801080f3a2f3b14b4a7244a660e
ireneq101 <ireneqomoregbee@gmail.com>
2024-02-20 12:13:40 +0100

    JUST

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,67 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10); 
+
+// console.log("Jonas");
+// console.log("23"); 
+
+// let firstname = "Jonas";
+// console.log(firstname);
+// console.log(firstname);
+// console.log(firstname); 
+
+// let javascriptIsFun = true
+// console.log(javascriptIsFun)
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// javascriptIsFun = "YES";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// let age = 30;
+// age = 31;
+
+// const birthyear = 1991;
+// birthyear = 1990;
+// const job;
+
+// const ageJonas = 2037 - 1991;
+// const ageSarah = 2037 -2018;
+// console.log(ageJonas, ageSarah);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas + 2, ageJonas/ 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 + 2 + 2
+
+const firstName = "Jonas";
+const lastName = "Schemedtmann";
+console.log(firstName + " " +  lastName);
+
+// these are assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x ++
+x --
+console.log(x)
+
+// comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
