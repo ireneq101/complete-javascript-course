a48d25644b8a5764a2dff19f2624c8e4014842d1
ireneq101 <ireneqomoregbee@gmail.com>
2024-03-02 13:58:54 +0100

    first

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -38,30 +38,99 @@
 // const ageSarah = 2037 -2018;
 // console.log(ageJonas, ageSarah);
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas + 2, ageJonas/ 10, 2 ** 3)
+// console.log(ageJonas + 2, ageJonas/ 10, 2 ** 3)
 // 2 ** 3 means 2 to the power of 3 = 2 + 2 + 2
 
-const firstName = "Jonas";
-const lastName = "Schemedtmann";
-console.log(firstName + " " +  lastName);
+// const firstName = "Jonas";
+// const lastName = "Schemedtmann";
+// console.log(firstName + " " +  lastName);
 
 // these are assignment operators
-let x = 10 + 5;
-x += 10;
-x *= 4;
-x ++
-x --
-console.log(x)
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x ++
+// x --
+// console.log(x)
 
 // comparison operators
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// strings and template literals:
+// 
+
+// if statements:
+const age = 15;
+
+// if (age >= 18) {
+//   console.log(`sarah can start driving license`);
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`sarah is too young. wait another ${yearsLeft} years`);
+// }
+
+// const birthyear = 2012;
+// let century;
+// if (birthyear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+// type conversion:
+// const inputYear = '1991';
+// console.log(inputYear + 18);// can not work.
+// console.log(Number(inputYear));
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Jonas'));// can convert to a number.
+// console.log(typeof NaN);// not a number.
+
+// console.log(string(23), 23);
+
+// type coercion:
+// console.log('i am ' + 23 + ' years old')
+// not all coercion do type coercion:
+// console.log('23' - '10' - 3); // js converted the strings to numbers.
+// console.log('23' * '2');
+// console.log('23' / '2');
+
+// truthy and falsy values:
+// 5 falsy values: 0, '', undefined, null, NaN.
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 0;
+// if (money) {
+//   console.log("Don't spend it all");
+// } else {
+//   console.log('You should get a job!')
+// }
+
+// const cash = 100;
+// if (cash) {
+//   console.log("Don't spend it all");
+// } else {
+//   console.log('You should get a job!')
+// }
